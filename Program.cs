b453ad0e2462a398374df424d2246641a8bebdbd
SynchraiMaple <maplecouper@gmail.com>
2022-12-13 12:27:14 +0000

    aaaaaaaa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Maple_s_12_Days_Of_Christmas;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+
+
+        string[] ords = ["zeroth", "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"]
+        string[] cards = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"]
+        string[] gifts = ["nothing", "partridge in a pear tree", "turtle doves", "french hens", "calling birds", "gold rings", "geese a-laying"]
+    }
+}
